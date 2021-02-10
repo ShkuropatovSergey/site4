139e0e852ee15a0fdf08e6a8eaccf6e13d188560
ShkuropatovSergey/site1 <axrro@mail.ru>
2021-02-11 00:06:37 +0300

    Работаю

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Document</title>
+    <title>Discover Amazing places in Japan</title>
 </head>
 <body>
-    xxxxxxxxxxxxx
+  <div class="c">aaaaaaaaaaaa</div>
+
+    <div class="as">aaaaaaaaaaaa</div>
 </body>
 </html>
